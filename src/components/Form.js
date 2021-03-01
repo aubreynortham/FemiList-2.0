9a6b1143d0cb4263cd770511093241ad85f9fcdc
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2021-02-28 19:05:03 -0500

    Render plain movie result data to the component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,10 @@ import React, { useState } from 'react';
 
 export default function Form() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [responseFinished, setResponseFinished] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [blankBechdelResults, setBlankBechdelResults] = useState(false);
+  const [movieData, setMovieData] = useState([]);
 
   // my CORS proxy reference: github.com/Rob--W/cors-anywhere/
   const customCorsProxy = 'https://serene-gorge-48466.herokuapp.com/';
@@ -12,35 +14,46 @@ export default function Form() {
   const urlBechdel = `${customCorsProxy}http://bechdeltest.com/api/v1/getMoviesByTitle?title=${searchTerm.replace(/\s/, '+')}`;
   const urlOMDB = 'https://omdbapi.com?i=';
 
-  const queryApiOMDB = (id) => {
-    axios
-      .get(`${urlOMDB}tt${id}${customOMDBKey}`)
-      .then((response) => {
-        if (response.status === 200) {
-          console.log(response.data);
-          return response.data;
-        }
-        return response;
-      });
+  const formattedTitle = (entry) => {
+    // HTML entities to plain text
+    const txt = document.createElement('textarea');
+    txt.innerHTML = entry;
+    // correct title stricture
+    if (txt.value.endsWith(', The')) {
+      txt.value = `The ${txt.value.substring(0, txt.value.length - 5)}`;
+    }
+    return txt.value;
   };
 
-  const queryApiBechdel = () => {
+  const queryAPIs = () => {
     axios
       .get(urlBechdel)
       .then((response) => {
-        if (response.status === 200) {
-          console.log(response.data);
-        } else {
+        if (response.status !== 200) {
           setBlankBechdelResults(true);
         }
         return response.data;
       })
       .then((data) => {
         if (data.length > 0) {
-          data.map((mov) => {
-            console.log(mov);
-            queryApiOMDB(mov.imdbid);
-            return mov;
+          data.map((el, idx) => {
+            axios
+              .get(`${urlOMDB}tt${el.imdbid}${customOMDBKey}`)
+              .then((response) => response.data)
+              // add relevant OMDB data to complement our Bechdel API response
+              .then((otherData) => {
+                el.Poster = otherData.Poster;
+                el.Genre = otherData.Genre;
+                el.imdbRating = otherData.imdbRating;
+                el.Rated = otherData.Rated;
+                el.Plot = otherData.Plot;
+                if (idx >= data.length - 1) {
+                  setSearchTerm('');
+                  setMovieData(data);
+                  setResponseFinished(true);
+                }
+              });
+            return data;
           });
         } else {
           setErrorMessage('No movie found by that title.');
@@ -52,7 +65,8 @@ export default function Form() {
     e.preventDefault();
     setErrorMessage('');
     setBlankBechdelResults(false);
-    queryApiBechdel();
+    setMovieData([]);
+    queryAPIs();
   };
 
   return (
@@ -69,9 +83,44 @@ export default function Form() {
       {blankBechdelResults && (
         <>
           <p>No results:</p>
-          <p>This site pulls in movie data from the Bechdel Test API. If you think a movie should be rated that is not yet in the database, contribute to the API <a id="hereLink" href="http://bechdeltest.com/add/">here</a>.</p>
+          <p>
+            This site pulls in movie data from the Bechdel Test API. If you think a movie should
+            be rated that is not yet in the database, contribute to the API{' '}
+            <a id="hereLink" href="http://bechdeltest.com/add/">here</a>
+            .
+          </p>
         </>
       )}
+      {responseFinished && movieData.length > 0 && movieData.map((movie, idx) => (
+        <article key={idx}>
+          {movie.Poster === 'N/A' ? (
+            <img src='http://i.imgur.com/ddETfiC.jpg' alt='Poster not available' />
+          ) : (
+            <img src={movie.Poster} alt={`${movie.title} poster`} />
+          )}
+          <h3>{formattedTitle(movie.title)}</h3>
+          <p>Genre: {movie.Genre}</p>
+          <p>Release Date: {movie.year}</p>
+          <p>Parental Rating: {movie.Rated}</p>
+          <p>IMDB Rating: {movie.imdbRating} / 10</p>
+          <p>Plot: {movie.Plot}</p>
+          <p>Bechdel Score: {movie.rating} / 3</p>
+          {movie.rating === 0 && (
+            <p>This movie does not even have two female characters.</p>
+          )}
+          {movie.rating === 1 && (
+            <p>There are at least two female characters but they do not share any dialogue with each
+            other.</p>
+          )}
+          {movie.rating === 2 && (
+            <p>There are at least two female characters who share dialogue with each other at some
+            point in the film, but their only dialogue is about a man.</p>
+          )}
+          {movie.rating === 3 && (
+            <p>This movie passes all test criteria</p>
+          )}
+        </article>
+      ))}
     </form>
   );
 }
