e06521f5222c02fde482620aadc97c10dc8747ed
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2021-03-21 15:05:39 -0400

    Update axios queries to ensure full results are loaded, layout tweaks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import blob from '../images/blob.svg';
 
 export default function Form() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [responseFinished, setResponseFinished] = useState(false);
+  const [contentLoading, setContentLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [blankBechdelResults, setBlankBechdelResults] = useState(false);
   const [movieData, setMovieData] = useState([]);
+  const results = [];
 
   // my CORS proxy reference: github.com/Rob--W/cors-anywhere/
   const customCorsProxy = 'https://serene-gorge-48466.herokuapp.com/';
@@ -25,7 +27,36 @@ export default function Form() {
     return txt.value;
   };
 
+  // sort results by descending IMDB votes and display top 10
+  const sortRelevance = (arr) => {
+    const sorted = arr.sort((a, b) => (parseFloat(a.imdbVotes.replace(/,/g, '')) > parseFloat(b.imdbVotes.replace(/,/g, '')) ? -1 : 1));
+    return sorted.slice(0, 10);
+  };
+
+  const queryFinalAPI = (dataRes, el) => {
+    axios
+      .get(`${urlOMDB}tt${el.imdbid}${customOMDBKey}`)
+      .then((response) => {
+        if (response.status === 200 && response.data.Poster) {
+          el.poster = response.data.Poster;
+          el.genre = response.data.Genre;
+          el.imdbRating = response.data.imdbRating;
+          el.rated = response.data.Rated;
+          el.plot = response.data.Plot;
+          el.imdbVotes = response.data.imdbVotes;
+          results.push(el);
+          if (results.length === dataRes.length) {
+            setMovieData(sortRelevance(results));
+            // setSearchTerm('');
+            setContentLoading(false);
+          }
+        }
+        return response.data;
+      });
+  };
+
   const queryAPIs = () => {
+    setContentLoading(true);
     axios
       .get(urlBechdel)
       .then((response) => {
@@ -36,27 +67,10 @@ export default function Form() {
       })
       .then((data) => {
         if (data.length > 0) {
-          data.map((el, idx) => {
-            axios
-              .get(`${urlOMDB}tt${el.imdbid}${customOMDBKey}`)
-              .then((response) => response.data)
-              // add relevant OMDB data to complement our Bechdel API response
-              .then((otherData) => {
-                el.Poster = otherData.Poster;
-                el.Genre = otherData.Genre;
-                el.imdbRating = otherData.imdbRating;
-                el.Rated = otherData.Rated;
-                el.Plot = otherData.Plot;
-                if (idx >= data.length - 1) {
-                  setSearchTerm('');
-                  setMovieData(data);
-                  setResponseFinished(true);
-                }
-              });
-            return data;
-          });
+          data.map((el) => queryFinalAPI(data, el));
         } else {
           setErrorMessage('No movie found by that title.');
+          setContentLoading(false);
         }
       });
   };
@@ -70,15 +84,21 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Does your movie pass the <a href='#bechdel-test'>Bechdel Test</a>?</h2>
-      <input
-        type='text'
-        onChange={(e) => setSearchTerm(e.target.value)}
-        value={searchTerm}
-        placeholder='Search for a movie'
-      />
-      <button type='submit'>Search</button>
+    <form className='form' onSubmit={handleSubmit}>
+      <div className='form__header'>
+        <img className='form__header-blob' src={blob} alt='' />
+        <h2 className='form__header-title'>Does your movie pass the <a href='#bechdel-test'>Bechdel Test</a>?</h2>
+      </div>
+      <div className='form__controls'>
+        <input
+          type='text'
+          placeholder='Search for a movie'
+          className='form__input'
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+        />
+        <button type='submit' className='form__button' disabled={!searchTerm}>Search</button>
+      </div>
       {errorMessage}
       {blankBechdelResults && (
         <>
@@ -91,36 +111,40 @@ export default function Form() {
           </p>
         </>
       )}
-      {responseFinished && movieData.length > 0 && movieData.map((movie, idx) => (
-        <article key={idx}>
-          {movie.Poster === 'N/A' ? (
-            <img src='http://i.imgur.com/ddETfiC.jpg' alt='Poster not available' />
-          ) : (
-            <img src={movie.Poster} alt={`${movie.title} poster`} />
-          )}
-          <h3>{formattedTitle(movie.title)}</h3>
-          <p>Genre: {movie.Genre}</p>
-          <p>Release Date: {movie.year}</p>
-          <p>Parental Rating: {movie.Rated}</p>
-          <p>IMDB Rating: {movie.imdbRating} / 10</p>
-          <p>Plot: {movie.Plot}</p>
-          <p>Bechdel Score: {movie.rating} / 3</p>
-          {movie.rating === 0 && (
-            <p>This movie does not even have two female characters.</p>
-          )}
-          {movie.rating === 1 && (
-            <p>There are at least two female characters but they do not share any dialogue with each
-            other.</p>
-          )}
-          {movie.rating === 2 && (
-            <p>There are at least two female characters who share dialogue with each other at some
-            point in the film, but their only dialogue is about a man.</p>
-          )}
-          {movie.rating === 3 && (
-            <p>This movie passes all test criteria</p>
-          )}
-        </article>
-      ))}
+      {contentLoading && 'LOADING!'}
+      {movieData && movieData.length > 0 && (
+        <>
+          <h2>Showing {movieData.length} result{movieData.length > 1 && 's'} for {searchTerm}</h2>
+          {movieData.map((movie, idx) => (
+            <article key={idx}>
+              <img src={movie.poster} alt={`${movie.title} poster`} />
+              <h3>{formattedTitle(movie.title)}</h3>
+              <p>Genre: {movie.genre}</p>
+              <p>Release Date: {movie.year}</p>
+              <p>Parental Rating: {movie.rated}</p>
+              <p>IMDB Rating: {movie.imdbRating} / 10</p>
+              <p>Plot: {movie.plot}</p>
+              <p>Bechdel Score: {movie.rating} / 3</p>
+              {movie.rating === 0 && (
+                <p>This movie does not even have two female characters.</p>
+              )}
+              {movie.rating === 1 && (
+                <p>
+                  There are at least two female characters but they do not share any dialogue with
+                  each other.
+                </p>
+              )}
+              {movie.rating === 2 && (
+                <p>There are at least two female characters who share dialogue with each other, but
+                their only dialogue is about a man.</p>
+              )}
+              {movie.rating === 3 && (
+                <p>This movie passes all test criteria</p>
+              )}
+            </article>
+          ))}
+        </>
+      )}
     </form>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import logo from '../images/logo.png';
+import rosieBg from '../images/rosie.jpg';
 
 export default function Header() {
   return (
     <div className='header'>
-      <h1 className='header__title'>
-        Movie ratings for feminists.*
-      </h1>
-      <p>
-        *And for the rest of the population, because the media&#39;s representation of women
-        matters.
-      </p>
+      <img className='header__image' src={rosieBg} alt='Rosie the Riveter clutches a pair of movie tickets' />
+      <div className='header__content'>
+        <img className='header__logo' src={logo} alt='The FemiList' />
+        <div>
+          <h1 className='header__title'>
+            Movie ratings for feminists.*
+          </h1>
+          <p className='header__disclaimer'>
+            <span className='header__highlight'>
+              *And for the rest of the population, because the media&#39;s representation of women
+              matters.
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
