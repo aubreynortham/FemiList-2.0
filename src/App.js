7e6f748d540afce7c19c4641e7cfad912df45350
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2021-03-21 23:21:02 -0400

    Add footer attributions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Bechdel from './components/Bechdel';
+import Footer from './components/Footer';
 import Form from './components/Form';
 import Header from './components/Header';
 import './scss/styles.scss';
@@ -7,9 +8,12 @@ import './scss/styles.scss';
 export default function App() {
   return (
     <>
-      <Header />
-      <Form />
-      <Bechdel />
+      <main>
+        <Header />
+        <Form />
+        <Bechdel />
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Bechdel() {
+  return (
+    <footer className='footer'>
+      <p className='footer__attributions'>
+      Website by <a href='https://github.com/aubreynortham/FemiList-2.0'>Aubrey</a>
+      <span className='footer__separator'>•</span>
+      Animated illustrations by <a href='https://storyset.com'>Storyset</a>
+      </p>
+    </footer>
+  );
+}
